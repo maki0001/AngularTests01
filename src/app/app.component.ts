f64a3775e43f983244e68b23b27903677cf29021
maki0001 <makiuchi3@gmail.com>
2019-12-10 13:12:33 +0900

    フリガナサンプル

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,34 @@
-import { Component, OnInit } from "@angular/core";
-import { AutoKana } from "vanilla-autokana";
+import { Component, OnInit, AfterViewInit } from "@angular/core";
+// import { AutoKana } from "vanilla-autokana";
+import * as AutoKana from "vanilla-autokana";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"]
 })
-export class AppComponent implements OnInit {
-  title = "CodeSandbox";
-  msg = "";
-  autoKana = null;
+export class AppComponent implements OnInit, AfterViewInit {
+  title: String = "CodeSandbox";
+  msg: String = "";
+  autoKana: any;
 
-  ngOnInit = () => {
-    this.autoKana = AutoKana.bind("#txtNameIn", "#msg", { katakana: true });
-  };
+  constructor() {
+    console.log("%s", "constructor");
+  }
 
-  onClick = (value: String) => {
+  ngOnInit() {
+    console.log("%s", "ngOnInit");
+    this.autoKana = AutoKana.bind("#name", "", { katakana: true });
+  }
+
+  ngAfterViewIni() {
+    console.log("%s", "ngAfterViewInit");
+  }
+
+  onClick(value: String) {
     console.log("%s", value);
+    // this.autoKana = AutoKana.bind("#name", "msg", { katakana: true });
+    console.log("%s", this.autoKana.getFurigana());
     this.msg = this.autoKana.getFurigana();
-  };
+  }
 }
